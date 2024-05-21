fbe1fdc923a30041c6edbe0ab55fe0b5dfc6cd0b
devapriyavs <150225899+devx4me@users.noreply.github.com>
2024-05-21 14:47:13 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,14 @@
+// function add(a,b){
+//     let c = a+b;
+//     console.log(c);
+// }
+// let fname="devapriya";
+// let lname=" subin";
+//  console.log(`${fname} ${lname}`)
+
+// add(1,2);
+
+function fullname(fname,lname){
+    console.log(`${fname} ${lname}`);
+}
+fullname("devapriya", "subin")
---
+++ script.js
@@ -0,0 +1,91 @@
+// // let a=1;
+// // let b=2;
+// // console.log(a+b);
+
+// // {
+// //     let a=10;
+// // }
+// // console.log(a);
+
+// // postfix incriment
+// // let a=1;
+// // let b=++a; 
+// // console.log(a);
+// // console.log(b);
+// // prefix
+// let d=1;
+// let s=++d;
+// console.log(d);
+// console.log(s);
+
+// a=10;
+// console.log(a);
+// fname="devapriya";
+// fname="dddd";
+
+// object
+// let car = {
+//     model: "mini cooper",
+//     color: "white"
+// };
+// console.log(car.model);
+// console.log(car.color);
+
+// let person = {
+//     fname:'devapriya',
+//     address:{
+//         street:"kdlr",
+//         place:"rose villa",
+//         pin:680664
+//     },
+//     age:19
+// }
+// console.log(person.address.pin);
+// console.log(person.age);
+
+// var toothbrush= new Object();
+// toothbrush.brand="pepsodent";
+// toothbrush.color="white";
+
+// console.log(toothbrush);
+
+// concatination
+let fname="devapriya";
+ let lname=" subin";
+  console.log(`${fname} and ${lname}`)
+
+// let m=5;
+// let n=6;
+// console.log(`the sum is ${m+n}`);
+// array
+// let fruits=["apple","mango","orange",23]
+// console.log(fruits);
+// fruits.push("grapes");
+// console.log(fruits);
+// console.log(fruits.length);
+// fruits[2]="pineapple";
+// console.log(fruits);
+// fruits.unshift("pear");
+// console.log(fruits)
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+
+
+// array of object
+let bsc=[
+    {
+    fname :"ammu",
+    age:17,
+},
+{
+    fname:"maalu",
+    age:19,
+},
+{
+    fname:"aghaa",
+    age:18,
+},
+];
+console.log(bsc[2].age);
